b9b41f3b828233425cab5f61fb4488dfd64dfd77
Pushpak <pushpak.jha@netapp.com>
2020-09-29 14:59:46 -0600

    Add a basic mysql database to ec2 project

--- ec2-ubuntu/ec2.tf
+++ ec2-ubuntu/ec2.tf
@@ -4,6 +4,9 @@ terraform {
       source = "hashicorp/aws"
       version = "~> 3.8.0"
     }
+    mysql = {
+      source = "terraform-providers/mysql"
+    }
   }
 }
 
@@ -12,7 +15,28 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "ec2_example" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+}
+
+resource "aws_db_instance" "mysql_example" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot = true
+}
+
+# Configure the MySQL provider based on the outcome of
+# creating the aws_db_instance.
+provider "mysql" {
+  endpoint = "${aws_db_instance.default.endpoint}"
+  username = "${aws_db_instance.default.username}"
+  password = "${aws_db_instance.default.password}"
 }
